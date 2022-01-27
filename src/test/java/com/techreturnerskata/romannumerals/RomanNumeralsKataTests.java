4b6c85e430b8274da6ce9c741a2b192ad1c0843e
lauraPrp <laura.porpiglia@gmail.com>
2022-01-27 22:00:41 +0000

    refactoring packages

--- src/test/java/com/techreturnerskata/romannumerals/RomanNumeralsKataTests.java
+++ src/test/java/com/techreturnerskata/romannumerals/RomanNumeralsKataTests.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-
 public class RomanNumeralsKataTests {
 
     private static RomanNumeralsKata romanNumeral;
