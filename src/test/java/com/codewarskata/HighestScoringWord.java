2df84f8acb7c1ed97ffd5b52c38ba3a56ac4aaf7
lauraPrp <laura.porpiglia@gmail.com>
2022-04-03 09:28:24 +0100

    HighestScoringWord refactor

--- src/test/java/com/codewarskata/HighestScoringWord.java
+++ src/test/java/com/codewarskata/HighestScoringWord.java
@@ -1,20 +1,36 @@
 package com.codewarskata;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class HighestScoringWord {
-    public static String high(String s) {
-        String[] words = s.split(" ");
-        int max = 0;
-        String maxWord = "";
-        for (String word : words) {
-            int sum = 0;
-            for (int i = 0; i < word.length(); i++) {
-                sum += word.charAt(i);
-            }
-            if (sum > max) {
-                max = sum;
-                maxWord = word;
+    final static String[] alphabet = {"", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+    public static List<String> split(String str) {
+        return Stream.of(str.split(" "))
+                .toList();
+    }
+
+    public static int getSingleScore(String word) {
+        int score = 0;
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = 1; j < alphabet.length; j++) {
+                if (String.valueOf(word.charAt(i)).equalsIgnoreCase(alphabet[j]))
+                    score = score + j;
             }
         }
-        return maxWord;
+        return score;
     }
+
+    public static String high(String s) {
+        List<String> wordsList = split(s);
+        String higherScore = "";
+        for (String word : wordsList) {
+            if (getSingleScore(higherScore) < getSingleScore(word))
+                higherScore = word;
+        }
+
+        return higherScore;
+    }
+
 }
