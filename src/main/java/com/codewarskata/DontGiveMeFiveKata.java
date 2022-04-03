3d7a7d8d539cb30ea4a367d52095053ffa657cb7
lauraPrp <laura.porpiglia@gmail.com>
2022-04-03 08:34:33 +0100

    DontGiveMeFive refactor

--- src/main/java/com/codewarskata/DontGiveMeFiveKata.java
+++ src/main/java/com/codewarskata/DontGiveMeFiveKata.java
@@ -1,17 +1,13 @@
 package com.codewarskata;
 
+import java.util.stream.IntStream;
+
 public class DontGiveMeFiveKata {
 
-    public static int dontGiveMeFive(int start, int end){
-        int i=start;
-        int count=0;
-        while(i<=end){
-            if(String.valueOf(i).indexOf("5")==-1){
-                count++;
-            }
-            i++;
-        }
-
-        return count;
+    public static long dontGiveMeFive(int start, int end) {
+        return IntStream.rangeClosed(start, end)
+                .filter(x -> !String.valueOf(x)
+                        .matches(".*5.*"))
+                .count();
     }
 }
